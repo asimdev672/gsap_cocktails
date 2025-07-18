37f9707a2e6a3d5141fee597b64ca5a648221109
asim <asim.dev672@gmail.com>
2025-07-18 17:22:40 +0500

    add lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
-import gsap from 'gsap';
+import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-
-import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
-import Cocktails from './components/Cocktails.jsx'
-import About from './components/About.jsx'
-import Art from './components/Art.jsx'
-import Menu from './components/Menu.jsx'
-import Contact from './components/Contact.jsx'
+import "lenis/dist/lenis.css";
+import { ReactLenis } from "lenis/react";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
+import { useEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
- return (
-	<main>
-	 <Navbar />
-	 <Hero />
-	 <Cocktails />
-	 <About />
-	 <Art />
-	 <Menu />
-	 <Contact />
-	</main>
- )
-}
+  const lenisRef = useRef();
 
-export default App
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+  return (
+    <main>
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
+  );
+};
+
+export default App;
